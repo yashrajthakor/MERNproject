2ed5d75bfa13c94f66c83b7136cb8462cd1a733b
Yashraj_thakor <yashrajthakor142@gmail.com>
2021-05-09 13:33:20 +0530

    Add middleware sample

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app= express();
 
+//middleware
+
+const middleware =(req,res,next)=>{
+console.log(`hello my middleware`);
+next();
+}
+
 app.get('/',(req, res)=>{
     res.send(`Hello world from the server`)
 })
-app.get('/about',(req, res)=>{
+app.get('/about', middleware,(req, res)=>{
+    console.log(`hello my aboutus`);
     res.send(`Hello about,world from the server`)
 })
 app.get('/contact',(req, res)=>{
