1f29f4e22375cac19e3490f9f46b8c1ad2536665
Yashraj_thakor <yashrajthakor142@gmail.com>
2021-05-10 10:58:55 +0530

    connect mongoDB with nodejs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app= express();
 
+const DB= 'mongodb+srv://yashrajthakor:Admin@123@cluster0.ckaho.mongodb.net/mernstack?retryWrites=true&w=majority';
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex : true,
+    useUnifiedTopology : true,    
+    useFindAndModify: false
+}).then(()=>{
+    console.log(`connection successful`);
+}).catch((err)=>{
+    console.log(`no connection`);
+});
 //middleware
 const middleware =(req,res,next)=>{
 console.log(`hello my middleware`);
